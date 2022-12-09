f33b5d2a9eed3c316c2142bfb3b8a935a4bdec03
annalenczowska <lenczowska.a@gmail.com>
2022-12-09 09:47:29 +0100

    Dodanie pętli.

--- src/MyName.java
+++ src/MyName.java
@@ -2,5 +2,18 @@ public class MyName {
     public static void main(String[] args) {
         String name = "Marcin";
         System.out.println("Moje imię to: " + name);
+
+        for (int i = 0; i < 3; i++) {
+            String guestName = "Zuzia";
+            String firstName;
+
+            firstName = switch (guestName) {
+                case "Kasia", "Katarzyna" -> "Katarzyna";
+                case "Zuza", "Zuzia" -> "Zuzanna";
+                case "Basia", "Barbara" -> "Barbara";
+                default -> "Guest";
+            };
+            System.out.println("Hello, " + firstName + "!");
+        }
     }
 }
